486b88a0f99be24ec75dc9f80f77bf983f6cfe98
김상인 <89206300+ksiisk99@users.noreply.github.com>
2023-06-25 23:02:44 +0900

    [Gold V] Title: 자두나무, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int[][][] dp = new int[3][T + 1][W + 2];
+        int[] arr = new int[T + 1];
+
+        for (int i = 1; i <= T; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 1; i <= T; i++) {
+            for (int j = 1; j <= W+1; j++) {
+                if (arr[i] == 1) {
+                    dp[1][i][j] = Math.max(dp[1][i-1][j], dp[2][i-1][j-1])+1;
+                    dp[2][i][j] = Math.max(dp[1][i-1][j-1], dp[2][i-1][j]);
+                } else {
+                    if(i==1 && j==1)continue;
+                    dp[1][i][j] = Math.max(dp[1][i-1][j], dp[2][i-1][j-1]);
+                    dp[2][i][j] = Math.max(dp[1][i-1][j-1], dp[2][i-1][j])+1;
+                }
+            }
+        }
+
+        int answer = 0;
+        for(int i=1;i<=W+1;i++){
+            answer=Math.max(answer, Math.max(dp[1][T][i], dp[2][T][i]));
+        }
+        System.out.print(answer);
+    }
+}
+
