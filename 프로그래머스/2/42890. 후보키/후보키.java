2f82974e3191b9120ddd18931eb38a0c78f5a4a5
김상인 <89206300+ksiisk99@users.noreply.github.com>
2023-11-28 17:16:16 +0900

    [level 2] Title: 후보키, Time: 13.46 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class Solution {
+    static ArrayList<HashSet<Integer>> candidateKey;
+    static int length;
+    static int answer;
+
+    public int solution(String[][] relation) {
+        answer=0;
+        candidateKey=new ArrayList<>();
+        length=relation[0].length;
+        for(int i=1;i<=length;i++){
+            makeSet(-1,i,0,new HashSet<Integer>(),relation);
+        }
+        return answer;
+    }
+
+    void makeSet(int index,int target,int count, HashSet<Integer> set,String[][] relation){
+        if(count==target){
+            if(!isUnique(set,relation))return;
+            for(HashSet<Integer>key:candidateKey){
+                if(set.containsAll(key)){
+                    return ;
+                }
+            }
+            candidateKey.add(set);
+            answer++;
+            return;
+        }
+        for(int i=index+1;i<length;i++){
+            HashSet<Integer>newSet=new HashSet<>(set);
+            newSet.add(i);
+            makeSet(i,target, count+1,newSet,relation);
+        }
+    }
+
+    boolean isUnique(HashSet<Integer> set, String[][] relation){
+        ArrayList<String> list=new ArrayList<>();
+        for(int i=0;i<relation.length;i++){
+            String temp="";
+            for(int index:set){
+                temp+=relation[i][index]+"/";
+            }
+            if(!list.contains(temp)){
+                list.add(temp);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
