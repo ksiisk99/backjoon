93dbfb069916587923ba9b17130bfcb7e743a9e0
김상인 <89206300+ksiisk99@users.noreply.github.com>
2024-02-24 21:27:53 +0900

    [Bronze I] Title: 옷걸이, Time: 488 ms, Memory: 76052 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27951. 옷걸이/옷걸이.java
+++ 백준/Bronze/27951. 옷걸이/옷걸이.java
@@ -8,23 +8,46 @@ public class Main {
     private static StringBuilder answer = new StringBuilder();
     private static int N, U, D;
     private static int[] 옷걸이;
+    private static int[] count = new int[4];
 
     public static void main(String[] args) throws IOException {
         input();
 
+        if (count[1] + count[3] < U || count[2] + count[3] < D) {
+            System.out.print("NO");
+            return;
+        }
+
+//        if (count[3] < Math.max(0, U - count[1]) + Math.max(0, D - count[2])) {
+//            System.out.print("NO");
+//            return;
+//        }
+
+        char[] answer = new char[N];
+
         for (int i = 0; i < N; i++) {
-            if ((옷걸이[i] == 1 || 옷걸이[i] == 3) && U > 0) {
+            if (옷걸이[i] == 1 && U > 0) {
                 U--;
-                answer.append("U");
-            } else if ((옷걸이[i] == 2 || 옷걸이[i] == 3) && D > 0) {
+                answer[i] = 'U';
+            } else if (옷걸이[i] == 2 && D > 0) {
                 D--;
-                answer.append("D");
+                answer[i] = 'D';
             }
         }
 
-        System.out.print(
-                N == answer.length() ? "YES\n" + answer.toString() : "NO"
-        );
+        for (int i = 0; i < N; i++) {
+            if (옷걸이[i] == 3) {
+                if (U > 0) {
+                    U--;
+                    answer[i] = 'U';
+                } else if (D > 0) {
+                    D--;
+                    answer[i] = 'D';
+                }
+            }
+        }
+        System.out.println("YES");
+        System.out.print(answer);
     }
 
     private static void input() throws IOException {
@@ -36,6 +59,7 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             옷걸이[i] = Integer.parseInt(st.nextToken());
+            count[옷걸이[i]]++;
         }
 
         st = new StringTokenizer(br.readLine());
@@ -44,3 +68,4 @@ public class Main {
     }
 
 }
+
