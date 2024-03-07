5d94074709802ac0e52a72f91f14d815b50c7775
김상인 <89206300+ksiisk99@users.noreply.github.com>
2024-03-07 21:20:20 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.13 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int answer = 0;
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0;i<friends.length;i++){
+            map.put(friends[i],i);
+        }
+        
+        int[] index = new int[friends.length];
+        int[][] record = new int[friends.length][friends.length];
+        
+        for(String gift: gifts) {
+            String[] cur = gift.split(" ");
+            index[map.get(cur[0])]++;
+            index[map.get(cur[1])]--;
+            record[map.get(cur[0])][map.get(cur[1])]++;         
+        }
+        
+        for(int i=0;i<friends.length; i++) {
+            int cnt = 0;
+            for(int j=0;j<friends.length; j++) {
+                if(i==j) continue;
+                if(record[i][j] > record[j][i]) cnt++;
+                else if(record[i][j]==record[j][i] && index[i] > index[j]) cnt++;
+            }
+            answer = Math.max(answer, cnt);
+        }
+        
+        return answer;
+    }
+}
